97a1624a08eeb0e5a494c787733734283ce012e9
Dakota Bayer <dakota.bayer@outlook.com>
2024-08-29 15:37:22 -0500

    page layout, added selector objects in js

---
+++ script.js
@@ -0,0 +1,17 @@
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const nameOutput = document.getElementById('pokemon-name');
+const idOutput = document.getElementById('pokemon-id');
+const weightOutput = document.getElementById('weight');
+const heightOutput = document.getElementById('height');
+const typeOutput = document.getElementById('types');
+const hpOutput = document.getElementById('hp');
+const attackOutput = document.getElementById('attack');
+const defenseOutput = document.getElementById('defense');
+const specialAttackOutput = document.getElementById('special-attack');
+const specialDefenseOutput = document.getElementById('special-defense');
+const speedOutput = document.getElementById('speed');
+
+searchButton.addEventListener('click', () => {
+    alert('clicked!');
+});
